517169b4c56e12a4f7f6fe69008d2d9e6da7dccd
sgrundhoefer <g!WXagN7g!WXagN74>
2022-08-12 12:09:22 +0200

    add: challenge

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "eu.sebaro"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "eu.sebaro.RodelbahnKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
---
+++ src/main/kotlin/Heimweg.kt
@@ -0,0 +1,10 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val field = File("src/main/resources/beispiel.txt").readLines()
+    println(solve())
+}
+
+private fun solve(): Int {
+    return 0
+}

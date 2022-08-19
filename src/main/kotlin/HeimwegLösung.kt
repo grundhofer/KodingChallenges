136bac5477bbf89e194d9a67708d43e3e5661fc3
sgrundhoefer <g!WXagN7g!WXagN74>
2022-08-19 14:31:48 +0200

    add: solution HeimwegLösung.kt

---
+++ src/main/kotlin/HeimwegLösung.kt
@@ -0,0 +1,22 @@
+import java.io.File
+
+fun main() {
+    val field = File("src/main/resources/wald.txt").readLines()
+    println(solve(field, 3))
+    println(solveAdvanced(field, 3 to 1))
+}
+
+private fun solve(field: List<String>, xValue: Int): Int {
+    val width = field[0].length
+    return field.indices.count {y ->
+        field[y][y * xValue % width] == '#'
+    }
+}
+
+private fun solveAdvanced(field: List<String>, vector: Pair<Int, Int>): Int {
+    val (dx, dy) = vector
+    val width = field[0].length
+    return field.indices.count { y ->
+        y % dy == 0 && field[y][y * dx / dy % width] == '#'
+    }
+}
